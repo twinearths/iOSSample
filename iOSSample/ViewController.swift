5b17fba828f017aeeb6b3841f55b5c91c03073a4
twinearths <info@twinearths.co.jp>
2022-01-25 02:17:11 +0900

    Implemnet viewcontroller

--- iOSSample/ViewController.swift
+++ iOSSample/ViewController.swift
@@ -8,26 +8,60 @@
 import UIKit
 
 
-class ViewController: UIViewController {
-
-    @objc func buttonTappe(sender:UIButton) {
-        let ctr = UIViewController(nibName: nil, bundle: nil)
-        ctr.title = "第２画面"
-        ctr.view.backgroundColor = .cyan
-        navigationController?.pushViewController(ctr, animated: true)
-    }
+class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         title = "第1画面"
         view.backgroundColor = .white
-        
-        let button = UIButton(frame: CGRect(origin: .zero, size: CGSize(width: 150, height: 44)))
-        button.setTitle("第２画面へ", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
-        button.addTarget(self, action: #selector(buttonTappe), for: .touchUpInside)
-        view.addSubview(button)
-        button.center = view.center
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    let listData:[String:[String]] = ["魚":["マグロ","カツオ","サンマ"],
+                                      "果物":["リンゴ","イチゴ","ナシ","ミカン","スイカ","オレンジ","イチジク","モモ","マンゴー"]]
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return listData.keys.count
+    }
+  
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+
+        let sectionTitle = Array(listData.keys)[section]
+        var frame = CGRect(origin: .zero, size: CGSize(width: tableView.frame.size.width, height: 40))
+        let view = UIView(frame: frame)
+        view.backgroundColor = .gray
+                           
+        frame.origin.x = 10
+        frame.size.width = frame.size.width - 10
+        let lbl = UILabel(frame: frame)
+        lbl.text = sectionTitle
+        lbl.textColor = .white
+        lbl.font = UIFont.boldSystemFont(ofSize: 20.0)
+
+        view.addSubview(lbl)
+        
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+           return 40
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        let key = Array(listData.keys)[section]
+        return listData[key]?.count ?? 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let key = Array(listData.keys)[indexPath.section]
+        let data = listData[key]
+        cell.textLabel?.text = data?[indexPath.row]
+
+        return cell
     }
 }
 

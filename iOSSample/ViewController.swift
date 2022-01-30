53c1733b23c3c4bc50390d5e61dca2afece13f3f
twinearths <info@twinearths.co.jp>
2022-01-30 17:28:54 +0900

    implement pageviewcontroller

--- iOSSample/ViewController.swift
+++ iOSSample/ViewController.swift
@@ -7,18 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    var myControllers = [UIViewController]()
 
     @IBAction func pushButton(sender:UIButton) {
-        let ctr = UIViewController(nibName: nil, bundle: nil)
-        ctr.title = "第２画面"
-        ctr.view.backgroundColor = .cyan
-        navigationController?.pushViewController(ctr, animated: true)
+        presentPageVC()
     }
     
     override func viewDidLoad() {
-        title = "第1画面"
         super.viewDidLoad()
+        
+        let colors:[UIColor] = [.cyan, .orange, .systemBlue]
+        for color in colors {
+            
+            let vc = UIViewController()
+            vc.view.backgroundColor = color
+            myControllers.append(vc)
+        }
+    }
+    
+    func presentPageVC() {
+        
+        guard let first = myControllers.first else {
+            return
+        }
+        let pvc = UIPageViewController(transitionStyle: .pageCurl,
+                                      navigationOrientation: .horizontal,
+                                      options: nil)
+        pvc.delegate = self
+        pvc.dataSource = self
+        
+        pvc.setViewControllers([first],
+                              direction: .forward,
+                              animated: true,
+                              completion: nil)
+        
+        present(pvc, animated: true)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let index = myControllers.firstIndex(of: viewController), index > 0 else {
+            return nil
+        }
+
+        let before = index - 1
+        return myControllers[before]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = myControllers.firstIndex(of: viewController), index < (myControllers.count - 1)  else {
+            return nil
+        }
+
+        let after = index + 1
+        return myControllers[after]
     }
 }
 
